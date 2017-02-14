a8e31f96185c96ba21b7092f7c700feb8a5d584a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-13 21:18:18 -0800

    parsing exercise

---
+++ parsing.py
@@ -0,0 +1,52 @@
+# grocery_string = "item:apples,quantity:4,price:1.50\n"
+# split_data = grocery_string.split(",")
+# split_item = split_data[0]
+# item_list = split_item.split(":")
+# item = item_list[1]
+
+# print item
+
+# my_name = "Maria"
+# print list(my_name)
+
+
+# string = "1,2,3,4,5"
+# string_to_integer = string.split(",")
+# print string_to_integer
+# for number in string_to_integer:
+#     print int(number)
+ 
+# string = "One fish two fish red fish blue fish"
+# string = string.lower() + " "
+# fish_list = string.split(" fish ")
+# print fish_list[:-1]
+
+
+# grocery_string = "item:apples,quantity:4,price:1.50\n"
+# split_data = grocery_string.split(",")
+
+# quantity_data = split_data[1]
+# quantity_list = quantity_data.split(":")
+# quantity = int(quantity_list[1])
+
+# price_data = split_data[2].strip()
+# price_list = price_data.split(":")
+# price = float(price_list[1])
+
+# print "Total bill is $" + str(price * quantity)
+
+
+
+# grocery_list = ["item:apples,quantity:4,price:1.50\n",
+#                 "item:pears,quantity:5,price:2.00\n",
+#                 "item:cereal,quantity:1,price:4.49\n"]
+
+# join_grocery_list = "".join(grocery_list).rstrip()
+# print join_grocery_list
+# split_grocery_list = 
+
+
+# for loop for the item total of each line
+# outside total
+# add inside total to outside total
+# print the outside total
